03c70667311930681bd34afa8f2ef475a0b8e65b
Mia <maria.sgr@gmail.com>
2021-06-22 19:02:00 +0200

    Improve service

--- marvel-react/src/services/searchService.js
+++ marvel-react/src/services/searchService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-export const searchComic = (title) => {
-    return fetch(`v1/query?title=${title}&publisher=marvel`)
+export const searchComic = (title, publisher = marvel) => {
+    return fetch(`v1/query?title=${title}&publisher=${publisher}`)
     .then((response) => {
         return response.json();
     })
@@ -11,8 +11,8 @@ export const searchComic = (title) => {
     });
 }
 
-export const searchComic = (title) => {
-    return axios.get(`v1/query?title=${title}&publisher=marvel`, { method: 'get', mode: 'no-cors', })
+export const searchComic = (title, publisher = marvel) => {
+    return axios.get(`v1/query?title=${title}&publisher=${publisher}`, { method: 'get', mode: 'no-cors', })
     .then(response => {
         const { data } = response;
         return data;
